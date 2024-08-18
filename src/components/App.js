a7ded3de0d8e9851ec1f5e24c13ebd50a3adaa4c
AryaAnish121 <anishkumarsarmi@gmail.com>
2024-08-18 17:30:45 +0530

    add personal touch and make responsive

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="heading">
-        <h1>Question Paper Editor</h1>
+        <h1>Question Paper Editor 🪶</h1>
       </div>
       <Questions />
     </div>
